cc8c4076eeee7448b170a6710835bc8d31b524e3
Bendi11 <bkliebmann@gmail.com>
2023-01-17 14:29:25 -0800

    Added flight computer for aircraft

--- Control Surfaces/Primary.cs
+++ Control Surfaces/Primary.cs
@@ -1,6 +1,7 @@
 using Sandbox.ModAPI.Ingame;
 using VRage.Game.ModAPI.Ingame.Utilities;
 using System;
+using VRageMath;
 
 namespace IngameScript {
     /// <summary>
@@ -8,16 +9,27 @@ namespace IngameScript {
     /// </summary>
     public class Surface {
         public PID PID;
-    
+
         float _maxAngle;
         float _defaultAngle;
         float _angle;
         IMyMotorStator _rotor;
         MyIni _ini;
 
+        Vector3 _sensitivity;
+
+        public const string
+            SECTION = "surface",
+            DEFAULT_ANGLE = "default-deg",
+            MAX_ANGLE = "lim-deg",
+
+            PITCH = "pitch",
+            YAW = "yaw",
+            ROLL = "roll";
+
         public float AngleRad {
             get { return _angle; }
-            set { _angle = Math.Min(MaxAngleRad, Math.Max(value, -MaxAngleRad)); }
+            set { _angle = Math.Min(MaxAngleRad, Math.Max(value + _defaultAngle, -MaxAngleRad)); }
         }
         const float DEG2RAD = (float)Math.PI / 180, RAD2DEG = 180 / (float)Math.PI;
         public float AngleDeg {
@@ -29,8 +41,8 @@ namespace IngameScript {
             get { return _maxAngle; }
             set {
                 _maxAngle = value;
-                _rotor.UpperLimitRad = _maxAngle;
-                _rotor.LowerLimitRad = -_maxAngle;
+                _rotor.UpperLimitRad = _defaultAngle + _maxAngle;
+                _rotor.LowerLimitRad = _defaultAngle - _maxAngle;
             }
         }
 
@@ -45,8 +57,19 @@ namespace IngameScript {
                 Log.Panic($"Failed to parse custom data for control surface {_rotor.DisplayNameText}");
             }
 
-            MaxAngleDeg = 22;
-            AngleRad = 0F;
+            _defaultAngle = FlightController.IniVal(_ini, SECTION, DEFAULT_ANGLE, 0) * DEG2RAD;
+            MaxAngleDeg = FlightController.IniVal(_ini, SECTION, MAX_ANGLE, 22);
+            _sensitivity.X = FlightController.IniVal(_ini, SECTION, PITCH);
+            _sensitivity.Y = FlightController.IniVal(_ini, SECTION, YAW);
+            _sensitivity.Z = FlightController.IniVal(_ini, SECTION, ROLL);
+            AngleRad = _defaultAngle;
+        }
+        
+        /// <summary>
+        /// Feed a primary control vector of (pitch, yaw, roll) into the angle of this rotor
+        public void FeedInput(Vector3 primaryControl) {
+            float ratio = (primaryControl * _sensitivity).Sum;
+            AngleRad = _maxAngle * ratio;
         }
 
         public void Update() {
--- lib/Log/Program.cs
+++ lib/Log/Program.cs
@@ -54,4 +54,61 @@ namespace IngameScript {
             }
         }
     }
+    
+    /// <summary>
+    /// Write text onto LCD panels with automatic screen wrapping and font sizing
+    /// </summary>
+    public class LcdWriter {
+        public IMyTextSurface LCD;
+        public bool Wrap = true;
+        readonly Vector2 _bounds, _defaultCharSize;
+        readonly StringBuilder _sb = new StringBuilder();
+        const string FONT = "Monospace";
+        int _lines = 0;
+        
+        /// <summary>
+        /// How many lines the LCD should be able to fit, adjusts font size
+        /// </summary>
+        public float FitLines {
+            set { LCD.FontSize = FitLines / value; }
+
+            get { return _bounds.Y / _defaultCharSize.Y; }
+        }
+
+        public float FitCols {
+            set { LCD.FontSize = FitCols / value; }
+
+            get { return _bounds.X / _defaultCharSize.X; }
+        }
+
+        public void Fit(float lines, float cols) {
+            var fLines = FitLines / lines;
+            var fCols = FitCols / cols;
+            LCD.FontSize = Math.Min(fLines, fCols);
+        }
+
+        public LcdWriter(IMyTextSurface lcd) {
+            _sb.Append('A');
+            LCD = lcd;
+            LCD.Font = FONT;
+            LCD.FontSize = 1;
+            LCD.TextPadding = 0F;
+            _defaultCharSize = LCD.MeasureStringInPixels(_sb, FONT, 1);
+        }
+        
+        public void Write(string msg) {
+            if(_lines >= _bounds.Y) {
+                LCD.WriteText("", false);
+                _lines = 0;
+            }
+            
+            do {
+                int len = Math.Min((int)_bounds.X, msg.Length);
+                LCD.WriteText(msg.Substring(0, len), true);
+                msg = msg.Substring(len);
+                LCD.WriteText("\n", true);
+                _lines += 1;
+            } while(Wrap && (msg.Length >= 0));
+        }
+    }
 }
