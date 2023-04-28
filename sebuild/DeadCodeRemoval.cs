84050fe4eb808c0089458e90052c9dbabf3daf47
Bendi11 <bkliebmann@gmail.com>
2023-04-28 11:53:28 -0700

    Added dead code removal

--- sebuild/DeadCodeRemoval.cs
+++ sebuild/DeadCodeRemoval.cs
@@ -1,5 +1,6 @@
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.FindSymbols;
 
 namespace SeBuild;
@@ -15,11 +16,55 @@ public class DeadCodeRemover {
         _proj = proj;
     }
 
-    class DeadCodeRewriter: CSharpSyntaxRewriter {
-        
+    public static async Task<(Solution, Project)> Build(Solution sln, Project proj) =>
+        await new DeadCodeRemover(sln, proj).Build();
+
+    public async Task<(Solution, Project)> Build() {
+        foreach(var doc in _proj.Documents) {
+            var syntax = await doc.GetSyntaxRootAsync() as CSharpSyntaxNode;
+            if(syntax is null) { continue; }
+            
+            var removed = Run(syntax);
+            if(removed is null) { continue; }
+            _sln = _sln.WithDocumentSyntaxRoot(doc.Id, removed);
+        }
+
+        return (_sln, _sln.GetProject(_proj.Id) ?? throw new Exception("Should never be null"));
     }
 
-    private async Task<bool> IsSymbolReferenced(ISymbol sym) {
+    public SyntaxNode? Run(CSharpSyntaxNode node) =>
+        node.Accept(new DeadCodeRewriter(this));
+
+    class DeadCodeRewriter: CSharpSyntaxRewriter {
+        DeadCodeRemover _parent;
+
+        public DeadCodeRewriter(DeadCodeRemover parent) {
+            _parent = parent;
+        }
+
+        public override SyntaxNode? VisitMethodDeclaration(MethodDeclarationSyntax node) =>
+            Referenced(node) ? node : null;
+
+        public override SyntaxNode? VisitClassDeclaration(ClassDeclarationSyntax node) =>
+            Referenced(node) ? node : null;
+
+        bool Referenced(CSharpSyntaxNode node) =>
+            Task.Run(async () => await _parent.IsSyntaxReferenced(node)).Result;
+    }
+
+    async Task<ISymbol?> GetSymbol(SyntaxNode node) {
+        var comp = await _proj.GetCompilationAsync();
+        var sema = comp?.GetSemanticModel(node.SyntaxTree);
+
+        return sema?.GetDeclaredSymbol(node);
+    }
+
+    public async Task<bool> IsSyntaxReferenced(SyntaxNode node) {
+        var symbol = await GetSymbol(node);
+        return symbol is null ? true : await IsSymbolReferenced(symbol);
+    }
+
+    public async Task<bool> IsSymbolReferenced(ISymbol sym) {
         if(_alive.ContainsKey(sym)) { return _alive[sym]; }
         
         bool alive = false;
@@ -34,10 +79,7 @@ public class DeadCodeRemover {
                 var node = sourceTree?.FindNode(loc.Location.SourceSpan);
                 if(node is null) { continue; }
     
-                var comp = await _proj.GetCompilationAsync();
-                var sema = comp?.GetSemanticModel(sourceNode);
-
-                var symbol = sema?.GetDeclaredSymbol(node);
+                var symbol = await GetSymbol(node); 
                 if(symbol is not null && (alive = await IsSymbolReferenced(symbol))) {
                     break;
                 }
